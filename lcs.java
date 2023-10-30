956695f3dbda3aeb0eb3f451896c45f1651b1fb4
Khushi Singh <khushi.singh092001@gmail.com>
2023-10-30 23:16:12 +0530

    Implemented lcs in java

---
+++ lcs.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    static int lcs(String X, String Y, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (X.charAt(m - 1) == Y.charAt(n - 1))
+            return 1 + lcs(X, Y, m - 1, n - 1);
+        else
+            return Math.max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
+    }
+
+    public static void main(String[] args) {
+        String S1 = "AGGTAB";
+        String S2 = "GXTXAYB";
+        int m = S1.length();
+        int n = S2.length();
+
+        System.out.println("Length of LCS is " + lcs(S1, S2, m, n));
+    }
+}
